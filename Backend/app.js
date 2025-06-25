8f0f5aaa5aaef3d74bbdd14aa4da3a1adc715707
unknown <boladeadeyemi@gmail.com>
2025-06-25 16:31:28 +0100

    adding 0.0.0.0 in my backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const groupMembers = [
-  'Bolade', 'Felix', 'Idowu', 'James', 'Agape', 'Oyerinde',
-  'John', 'Folashade', 'Ifeoluwa', 'Paul', 'Peter', 'Peace',
-  'Esther', 'Covenant', 'Hassan', 'Ingrid'
+  "Bolade",
+  "Felix",
+  "Idowu",
+  "James",
+  "Agape",
+  "Oyerinde",
+  "John",
+  "Folashade",
+  "Ifeoluwa",
+  "Paul",
+  "Peter",
+  "Peace",
+  "Esther",
+  "Covenant",
+  "Hassan",
+  "Ingrid",
 ];
 
-app.get('/api/groupmembers', (req, res) => {
+app.get("/api/groupmembers", (req, res) => {
   res.json({ groupMembers });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on http://localhost:${PORT}`);
 });
